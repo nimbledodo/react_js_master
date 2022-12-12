aa7f7b58a97db963ff7fe76ec69794c52bc18186
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-12-12 12:48:11 +0900

    #3.5 Forms
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLInputElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="red" borderColor="black" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          type="text"
+          placeholder="username"
+          onChange={onChange}
+        />
+        <button> Log in </button>
+      </form>
     </div>
   );
 }
