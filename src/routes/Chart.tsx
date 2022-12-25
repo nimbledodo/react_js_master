2197ab5aa3734ba8635b377937643856945bd91a
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-12-25 20:10:45 +0900

    #5.13 Price Chart p2
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,14 +1,68 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 
 interface ChartProps {
   coinId: string;
 }
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart.."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: true },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: true,
+            },
+            xaxis: {
+              axisBorder: { show: true },
+              axisTicks: { show: true },
+              labels: { show: true },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
