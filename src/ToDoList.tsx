48a260a9e6c50d3da25c483d12b6d037175c74a8
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-12-25 21:55:04 +0900

    #6.7 react-hook-form validation
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -20,13 +20,47 @@ function ToDoList() {
       </form>
     </div>
   ); */
-  const { register, watch } = useForm();
+  const { register, handleSubmit } = useForm();
+  const onValid = (data: any) => {};
   return (
     <div>
-      <form>
-        <input {...register("email")} placeholder="Email" />
-        <input {...register("firstname")} placeholder="First Name" />
-        <input {...register("lastname")} placeholder="Last Name" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("username", { required: true, minLength: 10 })}
+          placeholder="Username"
+        />
+        <input
+          {...register("firstname", { required: "First Name is required" })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("lastname", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("pw", {
+            required: true,
+            minLength: {
+              value: 5,
+              message: "Your password is too short",
+            },
+          })}
+          placeholder="Password"
+        />
+        <input
+          {...register("pw1", {
+            required: true,
+            minLength: {
+              value: 5,
+              message: "Your password is too short",
+            },
+          })}
+          placeholder="Confirm Password"
+        />
         <button>Add</button>
       </form>
     </div>
