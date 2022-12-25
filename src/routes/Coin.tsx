649714c1f6c5e52eca0c5d2cf11704d0d50ab540
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-12-25 19:59:19 +0900

    #5.12 Price Chart 1
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -225,7 +225,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
