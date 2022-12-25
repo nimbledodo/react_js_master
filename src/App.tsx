1290388ef3fd2ecd8c4c74dcab85180c1069e67e
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-12-25 21:26:38 +0900

    #6.5 clean up for todo
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import Router from "./Router";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { darkTheme, lightTheme } from "./theme";
-import { useRecoilValue } from "recoil";
-import { isDarkAtom } from "./atoms";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -69,14 +64,9 @@ a {
 `;
 
 function App() {
-  const isDark = useRecoilValue(isDarkAtom);
   return (
     <>
-      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-        <GlobalStyle />
-        <Router />
-        <ReactQueryDevtools initialIsOpen={true} />
-      </ThemeProvider>{" "}
+      <GlobalStyle />
     </>
   );
 }
