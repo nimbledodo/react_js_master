33db0979f1d223ec60f79a056b73b91c97d5a46a
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-12-25 21:44:08 +0900

    #6.6 React-hook-form basic
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
+  /*   const [toDo, setToDo] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
@@ -10,11 +11,22 @@ function ToDoList() {
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-  };
-  return (
+  }; */
+  /*   return (
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} placeholder="Write a todo" />
+        <button>Add</button>
+      </form>
+    </div>
+  ); */
+  const { register, watch } = useForm();
+  return (
+    <div>
+      <form>
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("firstname")} placeholder="First Name" />
+        <input {...register("lastname")} placeholder="Last Name" />
         <button>Add</button>
       </form>
     </div>
