a5492a1136043d96f8018b4ea1dbc51f6700947e
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-12-26 23:24:32 +0900

    #6.18 Enums
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,5 +1,5 @@
 import { useRecoilState, useRecoilValue } from "recoil";
-import { categoryState, toDoSelector } from "../atoms";
+import { Categories, categoryState, toDoSelector } from "../atoms";
 import CreateTodo from "./CreateTodo";
 import ToDo from "./ToDo";
 
@@ -13,9 +13,9 @@ function ToDoList() {
     <div>
       <h1>To Dos</h1>
       <select value={category} onInput={onInput}>
-        <option value="TO_DO">To Do</option>
-        <option value="DOING">Doing</option>
-        <option value="DONE">Done</option>
+        <option value={Categories.TO_DO}>To Do</option>
+        <option value={Categories.DOING}>Doing</option>
+        <option value={Categories.DONE}>Done</option>
       </select>
       <CreateTodo />
       {toDos?.map((toDo) => (
