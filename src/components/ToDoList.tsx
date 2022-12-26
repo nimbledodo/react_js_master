131979e92661fd2d13940ed5e365124b151f7fec
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-12-26 22:30:19 +0900

    #6.12 Refactoring toDos
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,23 @@
+import { useRecoilValue } from "recoil";
+import { toDoState } from "../atoms";
+import CreateTodo from "./CreateTodo";
+import ToDo from "./ToDo";
+
+function ToDoList() {
+  const toDos = useRecoilValue(toDoState);
+
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <CreateTodo />
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
