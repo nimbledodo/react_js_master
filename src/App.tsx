fc809daa56d6ea67ac22c2d332891fdcc7617078
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-12-29 21:01:00 +0900

    #8.5 drag part 1
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -14,28 +14,27 @@ const Box = styled(motion.div)`
   height: 200px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: white;
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  border-radius: 35px;
-  place-self: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
 const boxVariants = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { borderRadius: "100px", scale: 1 },
+  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 1 } },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} whileHover="hover" whileTap="click"></Box>
+      <Box
+        drag
+        variants={boxVariants}
+        whileHover="hover"
+        whileTap="click"
+        whileDrag="drag"
+      ></Box>
     </Wrapper>
   );
 }
