773c63f20d9041d24e49cedde458602698b1e1ae
Jeehyang Huh <jeehyang.huh@gmail.com>
2023-01-01 20:06:01 +0900

    #9.1 Header part 1
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" />
+        <Home />
+        <Route path="/tv" />
+        <Tv />
+        <Route path="/search" />
+        <Search />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
