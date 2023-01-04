f0c3580d13504ba24bfeb86d87acdf8ffaae6575
Jeehyang Huh <jeehyang.huh@gmail.com>
2023-01-04 23:33:12 +0900

    Add BrowserRouter basename
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <App />
+        <BrowserRouter basename={process.env.PUBLIC_URL} />
       </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>
