11f58b32651d0195d89227f9dd6741c5670eb345
Jeehyang Huh <jeehyang.huh@gmail.com>
2023-01-05 21:35:02 +0900

    Add baseurl to Router
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +15,6 @@ root.render(
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <App />
-        <BrowserRouter basename={process.env.PUBLIC_URL} />
       </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>
